412b35b168e9615f3adecac9fee56af6ff2774b5
lian0119 <lian0119@umn.edu>
2019-12-10 22:50:11 -0600

    Update fraud_func.py

--- fraud_func.py
+++ fraud_func.py
@@ -1,22 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ##############################################################################
 #  Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.   #
 #                                                                            #
